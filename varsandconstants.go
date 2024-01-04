6ce7ff721491b77b3029eb4c42f12205c0272ea0
root <blake.anneberg@gmail.com>
2024-01-03 22:04:54 -0700

    learning about go vars and consts

---
+++ varsandconstants.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+  const pi float64 = 3.14159265359
+ 
+  fmt.Println(pi)
+ 
+  var (
+    varA = 2
+    varB = 3
+  )
+ 
+  fmt.Println(varA, varB)
+ 
+  var Name string = "Blake A"
+
+  fmt.Println(len(Name))
+}
